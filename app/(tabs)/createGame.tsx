f5b7977f12b8601e9eab363f715cfa8d0298934f
filip <filip.witkowicz1@gmail.com>
2025-03-25 14:45:31 +0100

    add join and create game screens

---
+++ app/(tabs)/createGame.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function createGame() {
+
+  return (
+    <View>
+      <Text >Guessify</Text>
+      </View>
+  );
+}
+
+
---
+++ app/(tabs)/joinGame.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function joinGame() {
+
+  return (
+    <View>
+      <Text >Guessify</Text>
+      </View>
+  );
+}
+
+
