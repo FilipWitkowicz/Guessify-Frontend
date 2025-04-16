e820b2494259c1d9041832eacfa131049ddb633b
filip <filip.witkowicz1@gmail.com>
2025-04-16 16:57:15 +0200

    initialize socket connection

--- app/(tabs)/preGame.tsx
+++ app/(tabs)/preGame.tsx
@@ -1,9 +1,24 @@
 import { View, Text, StyleSheet } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import React from "react";
+import { io } from "socket.io-client";
+
 
 export default function PreGameScreen() {
   const { roomId } = useLocalSearchParams(); // Extract roomId from the parameters
+
+  React.useEffect(() => {
+    const socket = io("http://212.127.78.90:3000");
+    socket.emit("joinRoom", roomId);
+    console.log("Socket connected to room:", roomId);
+    alert(`Connected to room: ${roomId}`);
+
+    return () => {
+      socket.disconnect(); // Clean up the socket connection when the component unmounts
+    };
+  }, [roomId]);
+
+
 
   return (
     <View style={styles.container}>
